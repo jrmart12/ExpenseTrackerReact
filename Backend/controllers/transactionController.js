7f54636e9fdfa17a5ecc0d251cc00ac38c8c5e39
Jose Rodolfo Martinez Bardales <rodolfojose@live.com>
2020-03-22 19:48:12 -0700

    running both backend and frontend
    running both backend and frontend at the same time and installing axios for integration

--- Backend/controllers/transactionController.js
+++ Backend/controllers/transactionController.js
@@ -52,5 +52,26 @@ exports.addTransaction = async (req,res,next) =>{
 // route: DELETE /api/v1/transactions
 // public access
 exports.deleteTransaction = async (req,res,next) =>{
-    res.send('Delete transaction');
+    try {
+    const transaction = await Transaction.findById(req.params.id);
+
+    if(!transaction){
+        return res.status(404).json({
+            success: false,
+            error: 'No transaction found'
+        });
+    }
+
+    await transaction.remove();
+
+    return res.status(200).json({
+        success: true,
+        data: {}
+    });
+    } catch (e) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server error'
+        });    
+    }
 }
